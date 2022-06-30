cf34659b20cda0126583a91f9d9586f8b001baff
rauf <rauf.met@yandex.ru>
2022-06-30 10:06:56 +0500

    7.9 comleted

--- todoo/admin.py
+++ todoo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from todoo.models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+
+admin.site.register(Todo, TodoAdmin)
+
--- todoo/models.py
+++ todoo/models.py
@@ -1,3 +1,16 @@
+from email.policy import default
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(max_length=255)
+    created = models.DateField(auto_now_add=True)
+    complete = models.DateField(blank=True, auto_now_add=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
